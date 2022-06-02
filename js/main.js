fa9f44032fbaf24c00ace6616a0ac0b46d70fcf5
Daniel Leonel <eddfriends@hotmail.com>
2022-06-02 17:23:11 -0500

    Se agregó la parte del local storage

--- js/main.js
+++ js/main.js
@@ -98,13 +98,16 @@ agregar.addEventListener("click", (event)=>{
     contador++;
     document.getElementById("contadorProductos").innerHTML=contador;
     document.getElementById("precioTotal").innerHTML
+    localStorage.setItem("contadorProductos",contador);
     let precio = (Math.floor((Math.random() * 50)*100))/100;
     let cantidad = parseFloat(txtNumber.value);
     totalEnProductos += (cantidad<1)?Math.ceil(cantidad):parseInt(cantidad);
     document.getElementById("productosTotal").innerHTML = totalEnProductos; 
+    localStorage.setItem("productosTotal",totalEnProductos);
 
     costoTotal += (precio*cantidad);
     total.innerHTML=`$ ${costoTotal.toFixed(2)}`
+    localStorage.setItem("precioTotal", costoTotal.toFixed(2));
 
     let tmp = `<tr>
     <th scope="row">${contador}</th>
@@ -128,3 +131,21 @@ txtNombre.addEventListener("blur",(event)=>{
 txtNumber.addEventListener("blur", (event)=>{
     event.target.value = event.target.value.trim();
 })
+
+window.addEventListener("load", function(){
+
+    if(localStorage.getItem("contadorProductos")!=null){
+        contador = parseInt(localStorage.getItem("contadorProductos"));
+        document.getElementById("contadorProductos").innerHTML=contador;
+    }
+    if(localStorage.getItem("productosTotal")){
+        totalEnProductos = parseInt(localStorage.getItem("productosTotal"));
+        document.getElementById("productosTotal").innerHTML=totalEnProductos;
+    }
+    if(localStorage.getItem("precioTotal")!=null){
+        costoTotal = parseInt(localStorage.getItem("precioTotal"));
+        total.innerHTML=costoTotal;
+    }
+
+
+});
